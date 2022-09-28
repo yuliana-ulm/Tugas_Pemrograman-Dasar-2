03d3eb432e974b4ae9577d87823a4b4aa16c0db7
Yuliana <112523858+yuliana-ulm@users.noreply.github.com>
2022-09-29 02:57:56 +0800

    Add files via upload

---
+++ TugasSimari1.java
@@ -0,0 +1,16 @@
+public class TugasSimari1 {
+    public static void main(String[] args){
+        System.out.println(".--------------.");
+        System.out.println("| Biodata Diri |");
+        System.out.println("`--------------`");
+        System.out.println("Nama    : Yuliana");
+        System.out.println("Nim     : 2110131220001");
+        System.out.println("Prodi   : Pendidikan Ilmu Komputer");
+        System.out.println("Angkatan: 2021");
+        System.out.println("Alamat  : Jl Trans Kalimantan Km 21");
+        System.out.println("          Gang Haji Ilmi, Kab. Batola");
+        System.out.println("Hobi    : Berwisata Kuliner");
+        System.out.print("Mata Kuliah yang saya suka dan menurut saya, ");
+        System.out.print("saya menguasainya adalah Mata Kuliah PEMROGRAMAN DASAR 1");
+    }
+}
