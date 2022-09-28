f567bd8ed0396a3150794569527a8d4ed6456783
Yuliana <112523858+yuliana-ulm@users.noreply.github.com>
2022-09-29 02:30:44 +0800

    Update TugasKalkulatorF.java

--- TugasKalkulatorF.java
+++ TugasKalkulatorF.java
@@ -1,63 +1,63 @@
 import java.util.Scanner;
 
 public class TugasKalkulatorF{
-    public static void main(String[] args){
+    public static void main(String[] arguments){
         Scanner keyboard = new Scanner(System.in);
         String operasi;
-        double first, two, hasil;
+        double pertama, kedua, hasil;
 
 
-        System.out.print("Masukkan bilangan pertama = "); 
-        first = keyboard.nextInt();
+        System.out.print("Masukkan bilangan bulat pertama = "); 
+        pertama = keyboard.nextInt();
         
-        System.out.print("Masukkan bilangan kedua = ");
-        two = keyboard.nextInt();
+        System.out.print("Masukkan bilangan bulat kedua = ");
+        kedua = keyboard.nextInt();
 
-        System.out.print("operasi (+, -, x, /) = ");
+        System.out.print("Operasi (+, -, x, /) = ");
         operasi = keyboard.next();
         
             switch(operasi){
                 case "+":                           
-                    hasil = penjumlahan(first, two);
-                    System.out.println(first + " + " + two + " = " + hasil);
+                    hasil = penjumlahan(pertama, kedua);
+                    System.out.println(pertama + " + " + kedua + " = " + hasil);
                     break;
                 case "-":                           
-                    hasil = pengurangan(first, two);
-                    System.out.println(first + " - " + two + " = " + hasil);
+                    hasil = pengurangan(pertama, kedua);
+                    System.out.println(pertama + " - " + kedua + " = " + hasil);
                     break;
                 case "x":                           
-                    hasil = perkalian(first, two);
-                    System.out.println(first + " x " + two + " = " + hasil);
+                    hasil = perkalian(pertama, kedua);
+                    System.out.println(pertama + " x " + kedua + " = " + hasil);
                     break;
                 case "/":                           
-                    hasil = pembagian(first, two);
-                    System.out.println(first + " / " + two + " = " + hasil);
+                    hasil = pembagian(pertama, kedua);
+                    System.out.println(pertama + " / " + kedua + " = " + hasil);
                     break;
                 default:
-                    System.out.println("Operasi tidak ada!!!");
+                    System.out.println("Operasi tidak ada, Silahkan pilih yang lain");
                 // default:
             
         }
 
     }
-    static double penjumlahan(double x, double y){
+    static double penjumlahan(double a, double b){
         double hasil;
-        hasil = x + y;
+        hasil = a + b;
         return hasil;
     }
-        static double pengurangan(double x, double y){
+        static double pengurangan(double a, double b){
         double hasil;
-        hasil = x - y;
+        hasil = a - b;
         return hasil;
     }
-        static double perkalian(double x, double y){
+        static double perkalian(double a, double b){
         double hasil;
-        hasil = x * y;
+        hasil = a * b;
         return hasil;
     }
-        static double pembagian(double x, double y){
+        static double pembagian(double a, double b){
         double hasil;
-        hasil = x / y;
+        hasil = a / b;
         return hasil;
     }
 }
